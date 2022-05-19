3013e52568f37910fded36902b0f6bb29173374c
camij11 <100684629+camij11@users.noreply.github.com>
2022-05-19 22:33:55 +0200

    selection conto

--- src/main/webapp/GestoreHomeSimo.js
+++ src/main/webapp/GestoreHomeSimo.js
@@ -1,4 +1,3 @@
-let IDConto = null;
 
 window.addEventListener("load", () => {
 	if (sessionStorage.getItem("username") == null) {
@@ -52,53 +51,50 @@ function getContiUtente() {
 	);
 }
 
-document.getElementById("showinfoconto").addEventListener('click', (e) => {
-	var data = new FormData();
-	data.append("conto", IDConto);
-	makeCall("Post", 'SelezionaConto', data,
-		function(x) {
-			if (x.readyState == XMLHttpRequest.DONE) {
-				var message = x.responseText;
-				switch (x.status) {
-					case 200:
-						var content = JSON.parse(x.responseText);
-						document.getElementById("DettaglioContoIDConto").textContent = content[0].IDConto;
-						document.getElementById("DettaglioContoProprietario").textContent = content[0].proprietario;
-						document.getElementById("DettaglioContoSaldo").textContent = content[0].saldo;
-						const tBody = document.getElementById("trasferimenti");
-						for (let i = 0; i < content[1].length; i++) {
-							var IDT = content[1][i].IDTrasferimento;
-							var Data = content[1][i].data;
-							var Importo = content[1][i].importo;
-							var Causale = content[1][i].causale;
-							var IDContoOrigine = content[1][i].IDContoOrigine;
-							var IDContoDestinazione = content[1][i].IDContoDestinazione;
-							tBody.innerHTML += " <tr><td>" + IDT + "</td><td>" + Data + "</td><td>" + Importo + "</td><td>" + Causale + "</td><td>" + IDContoOrigine + "</td><td>" + IDContoDestinazione + "</td></tr>";
-						}
-						break;
-					case 400: // bad request
-						document.getElementById("errormessage").textContent = message;
-						break;
-					case 401: // unauthorized
-						document.getElementById("errormessage").textContent = message;
-						break;
-					case 404: // not found
-						document.getElementById("errormessage").textContent = message;
-						break;
-					case 500: // server error
-						document.getElementById("errormessage").textContent = message;
-						break;
+document.getElementById("selectionbutton").addEventListener('click', (e) => {
+	var form = e.target.closest("form");
+	if (form.checkValidity()) {
+		makeCall("Post", 'SelezionaConto', form,
+			function(x) {
+				if (x.readyState == XMLHttpRequest.DONE) {
+					var message = x.responseText;
+					switch (x.status) {
+						case 200:
+							var content = JSON.parse(x.responseText);
+							document.getElementById("DettaglioContoIDConto").textContent = content[0].IDConto;
+							document.getElementById("DettaglioContoProprietario").textContent = content[0].proprietario;
+							document.getElementById("DettaglioContoSaldo").textContent = content[0].saldo;
+							const tBody = document.getElementById("trasferimenti");
+							tBody.innerHTML = "";
+							for (let i = 0; i < content[1].length; i++) {
+								var IDT = content[1][i].IDTrasferimento;
+								var Data = content[1][i].data;
+								var Importo = content[1][i].importo;
+								var Causale = content[1][i].causale;
+								var IDContoOrigine = content[1][i].IDContoOrigine;
+								var IDContoDestinazione = content[1][i].IDContoDestinazione;
+								tBody.innerHTML += " <tr><td>" + IDT + "</td><td>" + Data + "</td><td>" + Importo + "</td><td>" + Causale + "</td><td>" + IDContoOrigine + "</td><td>" + IDContoDestinazione + "</td></tr>";
+							}
+							break;
+						case 400: // bad request
+							document.getElementById("errormessage").textContent = message;
+							break;
+						case 401: // unauthorized
+							document.getElementById("errormessage").textContent = message;
+							break;
+						case 404: // not found
+							document.getElementById("errormessage").textContent = message;
+							break;
+						case 500: // server error
+							document.getElementById("errormessage").textContent = message;
+							break;
+					}
 				}
 			}
-		});
-
-	if (document.getElementById("showinfoconto").textContent === "Tasto2") {
-		document.getElementById("infoconto").className = "show";
-		document.getElementById("showinfoconto").textContent = "AntiTasto2";
+		) 
 	} else {
-		document.getElementById("infoconto").className = "hide";
-		document.getElementById("showinfoconto").textContent = "Tasto2";
-	}
+		form.reportValidity();
+	  }
 });
 
 document.getElementById("inviatrasferimento").addEventListener('click', (e) => {
